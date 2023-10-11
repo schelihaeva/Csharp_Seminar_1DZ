c0d96d4064b58b8823725e30fecc32bda111351c
Anastasya <gbshell2022@gmail.com>
2023-10-11 12:53:16 +0300

    Решение задачи 4

--- Task_004/Program.cs
+++ Task_004/Program.cs
@@ -4,3 +4,14 @@
 // number = 5 -> 2    4
 
 // number = 8 -> 2    4    6    8
+Console.WriteLine("Введите число: ");
+int numTest = int.Parse(Console.ReadLine () ?? "");
+// Переберем все числа от 1 до числа numTest
+int cur = 2;
+Console.Write($"Четные числа от <1> до <{numTest}> :");  
+while (cur < numTest)
+{
+    Console.Write($" {cur}");
+    cur =cur +2;  
+}    
+Console.WriteLine("");
