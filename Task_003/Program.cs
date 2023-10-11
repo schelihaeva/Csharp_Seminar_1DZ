9b1eab6a12bb5069aabdeedaef6198b30bc86b25
Anastasya <gbshell2022@gmail.com>
2023-10-11 12:52:12 +0300

    Решение задачи3

--- Task_003/Program.cs
+++ Task_003/Program.cs
@@ -5,3 +5,9 @@
 // number = 3 -> Число 3 нечётное
 
 // number = 7 -> Число 7 нечётное
+Console.WriteLine("Введите число: ");
+int numTest = int.Parse(Console.ReadLine () ?? "");
+// Проверим на четность (т.е деление на два без остатка)
+if (numTest % 2 == 0 ) Console.WriteLine($"Число: {numTest} - Четное"); 
+else Console.WriteLine($"Число: {numTest} - Нечетное");  
+    
